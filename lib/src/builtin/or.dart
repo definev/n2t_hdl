0793dceab76ba46cf4b98f92c9f8e6dcc187961f
Definev <daiduong.workmail@gmail.com>
2024-04-11 02:36:34 +0700

    Refactor OrGate constructor to use named parameters

--- lib/src/builtin/or.dart
+++ lib/src/builtin/or.dart
@@ -6,27 +6,34 @@ import 'package:n2t_hdl/src/builtin/nand.dart';
 
 class OrGate extends ComponentGate {
   OrGate._({
-    super.name = 'OR',
-    super.inputCount = 2,
-    super.outputCount = 1,
-    super.connections = const [
-      [
-        LinkedConnection(fromIndex: 0, toComponent: 0, toIndex: 0),
-        LinkedConnection(fromIndex: 0, toComponent: 0, toIndex: 1),
-      ],
-      [
-        LinkedConnection(fromIndex: 1, toComponent: 1, toIndex: 0),
-        LinkedConnection(fromIndex: 1, toComponent: 1, toIndex: 1),
-      ],
-    ],
-    super.portNames = const PortNames(
-      inputNames: ['a', 'b'],
-      outputNames: ['out'],
-    ),
+    required super.name,
+    required super.inputCount,
+    required super.outputCount,
+    required super.connections,
     required super.componentIOs,
+    required super.portNames,
   });
 
-  factory OrGate() => OrGate._(componentIOs: _componentIOs);
+  factory OrGate() => OrGate._(
+        componentIOs: _componentIOs,
+        name: 'OR',
+        inputCount: 2,
+        outputCount: 1,
+        connections: const [
+          [
+            LinkedConnection(fromIndex: 0, toComponent: 0, toIndex: 0),
+            LinkedConnection(fromIndex: 0, toComponent: 0, toIndex: 1),
+          ],
+          [
+            LinkedConnection(fromIndex: 1, toComponent: 1, toIndex: 0),
+            LinkedConnection(fromIndex: 1, toComponent: 1, toIndex: 1),
+          ],
+        ],
+        portNames: const PortNames(
+          inputNames: ['a', 'b'],
+          outputNames: ['out'],
+        ),
+      );
 
   static List<ComponentIO> get _componentIOs => [
         ComponentIO(
