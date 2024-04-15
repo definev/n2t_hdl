3c0b2bbfcc20d854b3a8ef9525e7b446eb54ad73
Definev <daiduong.workmail@gmail.com>
2024-04-15 09:22:16 +0700

    Update import statements for part_connection.dart in gate_interpreter.dart and gate_kind.dart

--- lib/src/gate/gate_kind/gate_kind.dart
+++ lib/src/gate/gate_kind/gate_kind.dart
@@ -3,7 +3,7 @@ import 'package:n2t_hdl/src/builtin/component/connection.dart';
 import 'package:n2t_hdl/src/builtin/gate.dart';
 import 'package:n2t_hdl/src/gate/gate_blueprint.dart';
 import 'package:n2t_hdl/src/gate/gate_factory.dart';
-import 'package:n2t_hdl/src/gate/part_connection.dart';
+import 'package:n2t_hdl/src/gate/gate_kind/parts_gate/part_connection.dart';
 
 part 'builtin_gate.dart';
 part 'parts_gate.dart';
